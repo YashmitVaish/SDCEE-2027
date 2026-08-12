56adcf2aecdd9e67fab17d5a4032d5090401e6ba
Varun Gupta <varung202005@gmail.com>
2026-08-13 00:35:34 +0530

    fix

--- client/src/Components/Five/Five.js
+++ client/src/Components/Five/Five.js
@@ -6,15 +6,7 @@ const Five = () => {
       <div className="wrapper">
         <h3>Department of Chemical Engineering</h3>
         <p>
-          The Chemical Engineering Department at TIET offers a comprehensive and
-          dynamic education in chemical engineering, preparing students for
-          diverse opportunities in the field. The department’s focus on
-          research, innovation, and theory equips students with the necessary
-          knowledge and skills to develop economical and sustainable solutions
-          for utilizing materials and energy for the benefit of humanity. The
-          Department’s faculty is actively involved in research, with an
-          impressive track record of ~350 high-impact publications in SCI
-          journals from 2015 to 2023.
+        The DST-FIST-sponsored Department of Chemical Engineering at TIET offers a comprehensive and dynamic educational ecosystem designed to prepare students for diverse global opportunities. By seamlessly blending core theoretical frameworks with cutting-edge research and innovation, the department equips its graduates with the vital skills needed to develop economical, sustainable solutions for material and energy utilization for the benefit of society. The department’s faculty members are deeply invested in high-impact research, boasting a commendable record of approximately 350 influential publications in Science Citation Index (SCI) indexed journals over the past decade. This robust research culture is backed by substantial financial backing from various government bodies, securing competitive research grants totaling roughly Rs. 600 Lakhs since 2015. Additionally, the department's state-of-the-art infrastructure is further strengthened by a dedicated DST-FIST grant of approximately Rs. 190 Lakhs, cementing its status as a premier hub for advanced engineering study and technological development
         </p>
       </div>
     </div>
