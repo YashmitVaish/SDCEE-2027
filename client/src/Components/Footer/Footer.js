8e7ade785527351c6edb37039ae0e2fa96a25be5
Yashmit <yashmitvaish4454@gmail.com>
2026-08-15 14:03:19 +0530

    fix

--- client/src/Components/Footer/Footer.js
+++ client/src/Components/Footer/Footer.js
@@ -90,15 +90,6 @@ export default function Footer() {
           </ul>
         </div>
       </div>
-      <div className="footer-ack">
-        <p>
-          <strong>Acknowledgement:</strong> The Microsoft CMT service was
-          used for managing the peer-reviewing process for this conference.
-          This service was provided for free by Microsoft and they bore all
-          expenses, including costs for Azure cloud services as well as for
-          software development and support.
-        </p>
-      </div>
     </div>
   );
 }
